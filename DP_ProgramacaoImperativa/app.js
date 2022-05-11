11018ef14f3908129763ddf420a3dd22a6629014
Guilherme <guilhermeleite.vieira91@gmail.com>
2022-05-10 21:48:06 -0300

    inicio da atividade da mesa de trabalho aula 5

--- DP_ProgramacaoImperativa/app.js
+++ DP_ProgramacaoImperativa/app.js
@@ -1,6 +1,34 @@
-function PolCent (polegadas) {
-    return polegadas * 2,54
+ //Função para transformar Polegadas em Centímetros
+
+/* function PolCent (polegadas) {
+    return polegadas * 2.54
 }
 
-console.log(PolCent(2));
+console.log(PolCent(2)); */
 
+// Função para receber uma string e converter em URL
+
+/* function url (palavra) {
+    return "www." + palavra + ".com.br"
+}
+
+console.log(url('digitalhouse')); */
+
+// Função que receba a mesma palavra mais acrescente um (!) ao final
+
+/* function exclamacao (frase) {
+    return frase + "!"
+}
+
+console.log(exclamacao("Isso é uma função em execução"));
+ */
+
+// Função que calcula a idade de um cachorro levando em consideração 7anos humanos para 1 de cachorro
+
+/* function idadeCao (idadeCachorro) {
+    return idadeCachorro * 7 + " idade humana"
+}
+
+console.log(idadeCao(2)); */
+
+// Crie uma função que calcule a 
