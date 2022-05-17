17500d92586f8daff1bb7dc7c853a7641585024a
Guilherme <guilhermeleite.vieira91@gmail.com>
2022-05-16 21:08:29 -0300

    Mesa de trabalho aula 7

---
+++ DP_ProgramacaoImperativa/3 Semana/Aula 7/index.js
@@ -0,0 +1,26 @@
+// Exercício do PG mesa de trabalho aula 9
+
+console.log(!true);
+console.log(!false);
+/* console.log(!!false true);
+ */
+console.log(!!true);
+//console.log(!1); porque esse deu False?
+console.log(1);
+console.log(!!1);
+console.log(!0); // porque esse deu verdadeiro
+console.log(!!'');
+console.log('');
+console.log(!'');// porque verdadeiro?
+let x = 5;
+
+let y = 9;
+
+console.log(x < 10 &&  x==5);
+console.log(x > 9 ||  x===5);
+console.log(!(x===y));
+
+
+
+
+
