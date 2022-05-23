8a384fb8948b2d880b71f5e886af59a5c978a083
Guilherme <guilhermeleite.vieira91@gmail.com>
2022-05-23 20:53:08 -0300

    Adicionei os conteudos da aula 10

---
+++ DP_ProgramacaoImperativa/4 Semana/Aula 10/aula10.js
@@ -0,0 +1,39 @@
+let nome = 'Marcos'
+let verdade = true
+let lista =[1,2,3]
+//console.log(nome[2])
+//console.log(nome.length)
+//console.log(verdade.length)// não vai funcionar se o tipo não é String
+//console.log(lista.length)
+// o espaço também é contado 
+// ex 1,2,' '.
+
+// Mesa de trabalho : 
+//A. Acessar elementos específicos de um array
+
+/* let nomes = ['Guilherme', 'Francisco', 'João']
+let sobrenome = ['Leite', 'João', 'Vieira'] */
+
+/* console.log(nomes.length)
+console.log(nomes[2]) */
+
+// B. Modificar cada um dos elementos de um array e imprimi-los no console
+
+// C. Adicionar elementos a um array.
+// D. Extrair elementos de um array.
+// E. Comparar elementos de um array com os elementos de outro.
+
+/* let grupoDeAmigos = [ ["Harry", "Ron", "Hermione"], ["Spiderman", "Hulk", "Ironman"], ["Penélope Glamour", "Pierre Nodoyuna","Patán"] ]
+console.log(grupoDeAmigos[1][0]) */
+
+
+
+
+
+
+
+
+
+
+
+
