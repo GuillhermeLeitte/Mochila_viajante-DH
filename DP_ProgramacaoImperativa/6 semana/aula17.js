e2a0166eafad5db8acac6d958edaf197cfa9a413
Guilherme <guilhermeleite.vieira91@gmail.com>
2022-06-07 20:49:27 -0300

    aula 17

---
+++ DP_ProgramacaoImperativa/6 semana/aula17.js
@@ -0,0 +1,34 @@
+const valores = [1,2,3,4,5,6,7,8,9];
+
+// console.log( valores [0]);
+
+/* for ( let i=0; i < valores.length; i++){
+     console.log( valores[i]*2)
+}; */
+
+/* let tabuada = valores.map( function(element){
+    return element +" - presente"
+} );
+ */
+/* console.log( tabuada )
+console.log( valores ) */
+
+/* let novoValores = valores.filter(function(y) {
+    return y % 2 == 0;
+} ); */
+
+/* console.log( novoValores ); */
+
+/* let soma = novoValores.reduce(function(acumulador, valor){
+    console.log( acumulador, valor );
+    return acumulador + valor;
+});
+ */
+/* console.log(soma); */
+
+/* valores.forEach(function(element){
+    console.log(element+" * 2 = "+ element*2)
+}); */
+
+
+
