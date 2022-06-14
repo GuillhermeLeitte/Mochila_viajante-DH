f13485e8e35c65250217e07a01ea24c073bdcc67
Guilherme <guilhermeleite.vieira91@gmail.com>
2022-06-13 21:07:55 -0300

    Adicionei primeiraAplicação

---
+++ DP_ProgramacaoImperativa/7 semana/primeiraAplicacao.js
@@ -0,0 +1,76 @@
+/* const pessoa = [
+    {
+        altura: 1.82,
+        sexo:"M"
+    },
+    {
+        altura: 1.75,
+        sexo:"F"
+    }
+];
+
+console.log( pessoa[0].altura ); */
+
+const pessoa = [
+    {
+        altura: 1.78,
+        sexo:"M"
+    },
+    {
+        altura: 1.60,
+        sexo:"F"
+    },
+    {
+        altura: 1.65,
+        sexo:"F"
+    },
+    {
+        altura: 1.88,
+        sexo:"M"
+    },
+    {
+        altura: 1.70,
+        sexo:"M"
+    },
+    {
+        altura: 1.73,
+        sexo:"F"
+    },
+    {
+        altura: 1.65,
+        sexo:"M"
+    },
+    {
+        altura: 1.70,
+        sexo:"F"
+    },
+    {
+        altura: 1.71,
+        sexo:"M"
+    },
+    {
+        altura: 1.83,
+        sexo:"M"
+    },
+    {
+        altura: 1.84,
+        sexo:"M"
+    },
+    {
+        altura: 1.78,
+        sexo:"M"
+    },
+    {
+        altura: 1.82,
+        sexo:"M"
+    },
+    {
+        altura: 1.78,
+        sexo:"M"
+    },
+    {
+        altura: 1.97,
+        sexo:"M"
+    }
+];
+
