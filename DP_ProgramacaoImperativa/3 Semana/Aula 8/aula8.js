208879a200bc163a86ad7e2a80ea68bf215e6e76
Guilherme <guilhermeleite.vieira91@gmail.com>
2022-06-25 17:46:42 -0300

    Aula 8

---
+++ DP_ProgramacaoImperativa/3 Semana/Aula 8/aula8.js
@@ -0,0 +1,59 @@
+// 4. As    maçãs     custam     R$     0,30     cada     se     forem     compradas    menos     do     que     uma
+//     dúzia,     e     R$     0,25     se     forem     compradas     pelo     menos     doze.     Escreva     um
+//     programa     que     leia     o     número     de     maçãs     compradas,     calcule     e     escreva     o
+//     valor    total    da    compra.
+
+// 5. Tendo     como     entrada     a     altura     e     o     sexo     (codificado     da     seguinte     forma:
+//     1:feminino          2:masculino)     de     uma     pessoa,     construa     um     programa     que
+//     calcule    e    imprima    seu    peso    ideal,    utilizando    as    seguintes
+//     Fórmulas:
+//     - para    homens:    (72.7    *    Altura)    – 58
+//     - para    mulheres:    (62.1    *    Altura)    – 44.7
+
+
+
+// 1. Escreva um programa para ler 2 valores (considere que não serão informados valores iguais e escrever    o    maior    deles.
+
+/* function Maior (a=0, b=0){
+    if (a == b) {
+        console.log("Os valores são iguais")
+    }
+
+    if (a > b){
+        console.log(`O ${a} é maior que ${b}`)
+    } else {
+        console.log(`O ${b} é maior que ${a}`)
+    }
+
+}
+
+console.log(Maior(3,4)) */
+
+
+// 2. Escreva     um     programa     para     ler     o     ano     de     nascimento     de     uma     pessoa  e escrever     uma     mensagem     que     diga     se     ela     poderá     ou     não     votar     este     ano
+//     (não    é    necessário    considerar    o    mês    em    que    ela    nasceu).
+
+/* function Votar(anoNasci, anoAtual){
+    let podeVotar = anoAtual - anoNasci;
+    if (podeVotar >= 16){
+        console.log(`Você tem : ${podeVotar} anos, já pode votar `)
+    }
+};
+
+console.log(Votar(1984,2022)) */
+
+
+// 3. Escreva     um     programa     que     verifique     a     validade     de     uma     senha     fornecida
+//     pelo     usuário.     A     senha     válida     é     o     número     1234.    Devem     ser    impressas     as
+//     seguintes    mensagens:
+//     ACESSO    PERMITIDO    caso    a    senha    seja    válida.
+//     ACESSO    NEGADO    caso    a    senha    seja    inválida.
+
+function entrada (){
+    let senha = 1234
+    if senha
+}
+
+
+
+
