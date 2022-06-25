390b4e98efb6772d0add2ca8c37337676c8fef27
Guilherme <guilhermeleite.vieira91@gmail.com>
2022-06-25 18:20:11 -0300

    exercícios aula 8

--- DP_ProgramacaoImperativa/3 Semana/Aula 8/aula8.js
+++ DP_ProgramacaoImperativa/3 Semana/Aula 8/aula8.js
@@ -1,16 +1,3 @@
-// 4. As    maçãs     custam     R$     0,30     cada     se     forem     compradas    menos     do     que     uma
-//     dúzia,     e     R$     0,25     se     forem     compradas     pelo     menos     doze.     Escreva     um
-//     programa     que     leia     o     número     de     maçãs     compradas,     calcule     e     escreva     o
-//     valor    total    da    compra.
-
-// 5. Tendo     como     entrada     a     altura     e     o     sexo     (codificado     da     seguinte     forma:
-//     1:feminino          2:masculino)     de     uma     pessoa,     construa     um     programa     que
-//     calcule    e    imprima    seu    peso    ideal,    utilizando    as    seguintes
-//     Fórmulas:
-//     - para    homens:    (72.7    *    Altura)    – 58
-//     - para    mulheres:    (62.1    *    Altura)    – 44.7
-
-
 
 // 1. Escreva um programa para ler 2 valores (considere que não serão informados valores iguais e escrever    o    maior    deles.
 
@@ -43,16 +30,81 @@ console.log(Maior(3,4)) */
 console.log(Votar(1984,2022)) */
 
 
-// 3. Escreva     um     programa     que     verifique     a     validade     de     uma     senha     fornecida
+/* // 3. Escreva     um     programa     que     verifique     a     validade     de     uma     senha     fornecida
 //     pelo     usuário.     A     senha     válida     é     o     número     1234.    Devem     ser    impressas     as
 //     seguintes    mensagens:
 //     ACESSO    PERMITIDO    caso    a    senha    seja    válida.
 //     ACESSO    NEGADO    caso    a    senha    seja    inválida.
 
-function entrada (){
-    let senha = 1234
-    if senha
-}
+
+
+function entrada (Senha=0){
+    if (Senha != 1234) {
+        return "ACESSO NEGADO"
+    } else {
+        return "ACESSO PERMITIDO"
+    }
+};
+
+console.log(entrada(1243))
+console.log(entrada(1234)) */
+
+
+/* // 4. As    maçãs     custam     R$     0,30     cada     se     forem     compradas    menos     do     que     uma
+//     dúzia,     e     R$     0,25     se     forem     compradas     pelo     menos     doze.     Escreva     um
+//     programa     que     leia     o     número     de     maçãs     compradas,     calcule     e     escreva     o
+//     valor    total    da    compra.
+
+
+
+function Frutas (qtdMaças){
+    let preço = 0;
+    if (qtdMaças >= 12){
+        preço = qtdMaças * 0.25,
+        console.log(`Foram ${qtdMaças} maçãs, preço total: R$ ${preço} reais. `)
+    } else {
+        preço = qtdMaças * 0.30,
+        console.log(`Foram ${qtdMaças} maçãs, preço total: R$ ${preço} reais. `)
+    };
+};
+
+console.log(Frutas(12))
+console.log(Frutas(10)) */
+
+
+/* // 5. Tendo     como     entrada     a     altura     e     o     sexo     (codificado     da     seguinte     forma:
+//     1:feminino          2:masculino)     de     uma     pessoa,     construa     um     programa     que
+//     calcule    e    imprima    seu    peso    ideal,    utilizando    as    seguintes
+//     Fórmulas:
+//     - para    homens:    (72.7    *    Altura)    – 58
+//     - para    mulheres:    (62.1    *    Altura)    – 44.7
+
+function pesoIdeal (sexo=0, altura=0){
+    let Sexo = 0
+    let Peso = 0
+    
+    if (sexo == 1){
+        Sexo =  "feminino"
+    }
+    if (sexo == 2){
+        Sexo = "masculino"
+    }
+
+    if (Sexo == "feminino"){
+        Peso = (62.1 * altura) - 44.7,
+        console.log(`Seu peso ideal é : ${Peso.toFixed(2)}`)
+
+    }
+    if (Sexo == "masculino"){
+        Peso = (72.7 * altura) - 58,
+        console.log(`Seu peso ideal é : ${Peso.toFixed(2)}`)
+
+    }
+};
+
+console.log(pesoIdeal(1,1.59))
+console.log(pesoIdeal(2,1.79)) */
+
 
 
 
