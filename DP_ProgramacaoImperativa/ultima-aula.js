e5c395ff9285c057f9d7c1705ad9f612bcfe5f91
Guilherme <guilhermeleite.vieira91@gmail.com>
2022-07-04 19:43:03 -0300

    Ultima aula

---
+++ DP_ProgramacaoImperativa/ultima-aula.js
@@ -0,0 +1,26 @@
+// Passo 1
+
+const produtos = [
+    {
+        nome: "Celular",
+        valor : 1785.50,
+        qualidade : 8,
+        status : true
+    },
+    {
+        nome: "Tenis",
+        valor : 450.50,
+        qualidade : 9,
+        status : true
+    },
+    {
+        nome: "Mouse",
+        valor : 150.00,
+        qualidade : 4,
+        status : true
+    },
+    
+
+];
+
+console.log(produtos[1]);
